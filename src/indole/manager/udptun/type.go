59fa9e7fba203a2352774a881eba8a47bcc909df
AaronGarbut <AaronGarbut@protonmail.com>
2019-06-23 18:48:14 +0800

    1st dev

--- src/indole/manager/udptun/type.go
+++ src/indole/manager/udptun/type.go
@@ -18,17 +18,6 @@ func (thisptr *UDPTUN) Run() {
 	x := udp.Build(thisptr.udp)
 	y := tun.Build(&tun.Args{
 		Device: "tun0",
-		Exec: []string{
-			"ifconfig tun0 10.8.0.1/16 mtu 1400 up",
-			"iptables -t nat -A POSTROUTING -s 10.8.0.0/16 ! -d 10.8.0.0/16 -j MASQUERADE",
-			"iptables -A FORWARD -s 10.8.0.0/16 -m state --state RELATED,ESTABLISHED -j ACCEPT",
-			"iptables -A FORWARD -d 10.8.0.0/16 -j ACCEPT",
-		},
-		Exit: []string{
-			"iptables -t nat -D POSTROUTING -s 10.8.0.0/16 ! -d 10.8.0.0/16 -j MASQUERADE",
-			"iptables -D FORWARD -s 10.8.0.0/16 -m state --state RELATED,ESTABLISHED -j ACCEPT",
-			"iptables -D FORWARD -d 10.8.0.0/16 -j ACCEPT",
-		},
 	})
 	c := make(chan struct{}, 2)
 
